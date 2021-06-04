94e779488717d065402c0dc967d5c2e34a780409
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-06-04 16:12:52 +0100

    Create soManyMEMESaboutThis.py

---
+++ soManyMEMESaboutThis.py
@@ -0,0 +1,12 @@
+# calcuate the factorial of a given integer i.e 10!, 42! etc
+
+def soManyMEMESaboutThis(numb):
+    factorial = 1
+    for i in range(1, numb + 1):
+        factorial = factorial * i
+    print(factorial)
+
+
+soManyMEMESaboutThis(10)
+
+
