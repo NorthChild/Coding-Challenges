97df5c04b091012215a80dce3d7f1bf9977a072a
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-06-04 15:32:17 +0100

    Update printOddBoys.py

--- printOddBoys.py
+++ printOddBoys.py
@@ -3,10 +3,10 @@
 for i in range(100):
     i = i + 1
     if i % 2 == 1:
-        print(str(i) + '\n')
+        print(i)
     # this part is only if you think you're funny, otherwise, exclude it lol 
     else:
-        print('this is even, so yeah nah, not printing it lol \n')
+        print('this is even, so yeah nah, not printing it lol')
 
 
 
