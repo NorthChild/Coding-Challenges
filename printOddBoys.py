267536076c8505f76ca4665cba5d55f525e8f7ea
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-06-04 13:12:25 +0100

    Create printOddBoys.py

---
+++ printOddBoys.py
@@ -0,0 +1,12 @@
+# print odd numbers < 100
+
+for i in range(100):
+    i = i + 1
+    if i % 2 == 1:
+        print(str(i) + '\n')
+    # this part is only if you think you're funny, otherwise, exclude it lol 
+    else:
+        print('this is even, so yeah nah, not printing it lol \n')
+
+
+
