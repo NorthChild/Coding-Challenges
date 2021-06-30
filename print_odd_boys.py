72857741970795fdc07f09c097fe3fb73a4e0af3
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-06-30 11:38:48 +0100

    Update print_odd_boys.py

--- print_odd_boys.py
+++ print_odd_boys.py
@@ -4,9 +4,9 @@ for i in range(100):
     i = i + 1
     if i % 2 == 1:
         print(i)
-    # this part is only if you think you're funny, otherwise, exclude it lol 
-    else:
-        print('this is even, so yeah nah, not printing it lol')
+#     # this part is only if you think you're funny, otherwise, exclude it lol 
+#     else:
+#         print('this is even, so yeah nah, not printing it lol')
 
 
 
